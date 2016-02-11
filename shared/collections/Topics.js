a7c6875e89d068e9cb443f9dda27975d8c1b501c
Tim Wissel <twtimwissel5@gmail.com>
2016-02-12 00:58:15 +0100

    Added timestampable collection behaviour

--- shared/collections/Topics.js
+++ shared/collections/Topics.js
@@ -1,5 +1,11 @@
 Topics = new Mongo.Collection("Topics");
 
 Schemas.Topics = new SimpleSchema({
-
+    title: {
+        type: String
+    }
 });
+
+Topics.attachSchema(Schemas.Topics);
+
+Topics.attachBehaviour("timestampable");
